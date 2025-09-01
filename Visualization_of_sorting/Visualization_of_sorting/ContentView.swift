d9aecc8b88c79edab042301046d3faa3a9978852
Igor Terletskyi <ihorek12@gmail.com>
2025-09-01 13:18:33 +0300

    Initializing the Project
    Initializing the Project

---
+++ Visualization_of_sorting/Visualization_of_sorting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Visualization_of_sorting
+//
+//  Created by Igor Terletskyi on 01.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Visualization_of_sorting/Visualization_of_sorting/Visualization_of_sortingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Visualization_of_sortingApp.swift
+//  Visualization_of_sorting
+//
+//  Created by Igor Terletskyi on 01.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Visualization_of_sortingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
